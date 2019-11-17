df0bf6f49c24d00d8442602dca60ccf6e4cb8055
Michał Budzyński <michal@virtualdesign.pl>
2019-11-17 21:24:00 +0100

    :heartbeat: remove built files

--- public/js/config.js
+++
@@ -1,6 +0,0 @@
-export const SCREEN_WIDTH = 256;
-export const SCREEN_HEIGHT = 240;
-export const FRAMEBUFFER_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;
-export const CANVAS_ID = "nes-canvas";
-export const ROM_URL = "roms/tetris.nes";
-//# sourceMappingURL=config.js.map
