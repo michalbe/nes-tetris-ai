0295242f02489674859fa8e690d756cc393c68e0
Michał Budzyński <michal@virtualdesign.pl>
2019-11-30 13:23:00 +0100

    :ribbon: map all rotations into arrays

--- src/tetraminos.ts
+++ src/tetraminos.ts
@@ -63,7 +63,9 @@ export const next_tetraminos: {
     },
 };
 
-export const all_possible_rotations = {
+const all_possible_rotations: {
+    [key: string]: Array<string>;
+} = {
     L: ["0111010000000000", "0110001000100000", "0001011100000000", "0010001000110000"],
     S: ["0011011000000000", "0010001100010000"],
     O: ["0110011000000000"],
@@ -71,5 +73,22 @@ export const all_possible_rotations = {
     Z: ["0110001100000000", "0001001100100000"],
     J: ["0111000100000000", "0010001001100000", "0100011100000000", "0011001000100000"],
 };
+
+export const get_all_rotations = (() => {
+    let rotations: {[key: string]: Array<Array<Array<number>>>} = {};
+    return (name: string) => {
+        if (rotations[name]) {
+            return rotations[name];
+        }
+
+        let rotations_map = all_possible_rotations[name];
+        rotations[name] = rotations_map.map(schema =>
+            schema.match(/.{1,4}/g)!.map(row => row.split("").map(element => parseInt(element, 10)))
+        );
+
+        return rotations[name];
+    };
+})();
+
 // @ts-ignore
 window.tetraminos = tetraminos;
